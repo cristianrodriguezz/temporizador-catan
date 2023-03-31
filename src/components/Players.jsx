68d6b266e4b5d25287b762ff8b8b8f9ee94a06b8
adrodriguez <adrodriguez@labsxd.com>
2023-03-31 01:51:29 -0300

    fix lista de players en config

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -7,118 +7,128 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import IconGripper from "../icons/IconGripper";
 
 const PlayersList = ({
-  players,
-  playerId,
-  timeBankToMinute,
-  setPlayers,
-  renderPlayer,
+    players,
+    playerId,
+    timeBankToMinute,
+    setPlayers,
+    isStartGame,
 }) => {
-  const handleDragEnd = (result) => {
-    if (!result.destination) return;
-    const newItems = [...players];
-    const [reorderedItem] = newItems.splice(result.source.index, 1);
-    newItems.splice(result.destination.index, 0, reorderedItem);
-    setPlayers(newItems);
-  };
-  console.log(renderPlayer);
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+        const newItems = [...players];
+        const [reorderedItem] = newItems.splice(result.source.index, 1);
+        newItems.splice(result.destination.index, 0, reorderedItem);
+        setPlayers(newItems);
+    };
+    console.log(isStartGame);
 
-  return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="players">
-        {(droppableProvided) => (
-          <ul
-            {...droppableProvided.droppableProps}
-            ref={droppableProvided.innerRef}
-            className="timerListPlayer"
-            style={
-              players.length > 5
-                ? {
-                    gridTemplateColumns: "repeat(auto-fill, minmax(150px,1fr))",
-                  }
-                : null
-            }
-          >
-            {players?.map((player, index) => (
-              <Draggable
-                key={player.id}
-                draggableId={player.id.toString()}
-                index={index}
-                isDragDisabled={renderPlayer}
-              >
-                {(draggableProvided) => (
-                  <li
-                    {...draggableProvided.draggableProps}
-                    ref={draggableProvided.innerRef}
-                  >
-                    <motion.div
-                      initial={{ scale: 0 }}
-                      animate={{ scale: 1 }}
-                      transition={{
-                        type: "spring",
-                        stiffness: 260,
-                        damping: 20,
-                      }}
-                      className="player"
-                      style={stylePlayerSelected(player, playerId, renderPlayer)}
+    return (
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="players">
+                {(droppableProvided) => (
+                    <ul
+                        {...droppableProvided.droppableProps}
+                        ref={droppableProvided.innerRef}
+                        className="timerListPlayer"
+                        style={
+                            players.length > 5 && isStartGame
+                                ? {
+                                      gridTemplateColumns:
+                                          "repeat(auto-fill, minmax(150px,1fr))",
+                                  }
+                                : null
+                        }
                     >
-                      <div>
-                        <p style={{ fontSize: "2rem" }}>
-                          {playerId === player.id
-                            ? timeBankToMinute
-                            : useSecondsToString(player.timerBank)}
-                        </p>
-                        <p>{player.name}</p>
-                      </div>
-                      {renderPlayer ? null : (
-                        <div
-                          style={{ display: "grid", placeItems: "center" }}
-                          {...draggableProvided.dragHandleProps}
-                        >
-                          <IconGripper />
-                        </div>
-                      )}
-                    </motion.div>
-                  </li>
+                        {players?.map((player, index) => (
+                            <Draggable
+                                key={player.id}
+                                draggableId={player.id.toString()}
+                                index={index}
+                                isDragDisabled={isStartGame}
+                            >
+                                {(draggableProvided) => (
+                                    <li
+                                        {...draggableProvided.draggableProps}
+                                        ref={draggableProvided.innerRef}
+                                    >
+                                        <motion.div
+                                            initial={{ scale: 0 }}
+                                            animate={{ scale: 1 }}
+                                            transition={{
+                                                type: "spring",
+                                                stiffness: 260,
+                                                damping: 20,
+                                            }}
+                                            className="player"
+                                            style={stylePlayerSelected(
+                                                player,
+                                                playerId,
+                                                isStartGame
+                                            )}
+                                        >
+                                            <div>
+                                                <p style={{ fontSize: "2rem" }}>
+                                                    {playerId === player.id
+                                                        ? timeBankToMinute
+                                                        : useSecondsToString(
+                                                              player.timerBank
+                                                          )}
+                                                </p>
+                                                <p>{player.name}</p>
+                                            </div>
+                                            {isStartGame ? null : (
+                                                <div
+                                                    style={{
+                                                        display: "grid",
+                                                        placeItems: "center",
+                                                    }}
+                                                    {...draggableProvided.dragHandleProps}
+                                                >
+                                                    <IconGripper />
+                                                </div>
+                                            )}
+                                        </motion.div>
+                                    </li>
+                                )}
+                            </Draggable>
+                        ))}
+                        {droppableProvided.placeholder}
+                    </ul>
                 )}
-              </Draggable>
-            ))}
-            {droppableProvided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
+            </Droppable>
+        </DragDropContext>
+    );
 };
 
 const NotPlayers = ({ errorNotPlayers }) => {
-  return errorNotPlayers ? (
-    <p className="notPlayers" style={{ color: "red" }}>
-      Agregue jugadores para iniciar
-    </p>
-  ) : (
-    <p className="notPlayers">No hay jugadores agregados</p>
-  );
+    return errorNotPlayers ? (
+        <p className="notPlayers" style={{ color: "red" }}>
+            Agregue jugadores para iniciar
+        </p>
+    ) : (
+        <p className="notPlayers">No hay jugadores agregados</p>
+    );
 };
 
 export const Players = ({
-  players,
-  playerId,
-  timeBankToMinute,
-  errorNotPlayers,
-  setPlayers,
-  renderPlayer,
+    players,
+    playerId,
+    timeBankToMinute,
+    errorNotPlayers,
+    setPlayers,
+    isStartGame,
 }) => {
-  const hasPlayers = players?.length > 0;
+    const hasPlayers = players?.length > 0;
 
-  return hasPlayers ? (
-    <PlayersList
-      setPlayers={setPlayers}
-      players={players}
-      playerId={playerId}
-      timeBankToMinute={timeBankToMinute}
-      renderPlayer={renderPlayer}
-    />
-  ) : (
-    <NotPlayers errorNotPlayers={errorNotPlayers} />
-  );
+    return hasPlayers ? (
+        <PlayersList
+            setPlayers={setPlayers}
+            players={players}
+            playerId={playerId}
+            timeBankToMinute={timeBankToMinute}
+            isStartGame={isStartGame}
+        />
+    ) : (
+        <NotPlayers errorNotPlayers={errorNotPlayers} />
+    );
 };
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,227 +4,298 @@ import { useSecondsToString } from "../hooks/useSecondToMinute";
 import { styleButtonsTimer } from "../constants/styleButtonsTimer";
 import { motion } from "framer-motion";
 
-const Timer = ({ initialTime, players, renderPlayer }) => {
-  const [timeGame, setTimeGame] = useState(initialTime);
-  const [player, setPlayer] = useState(players);
-  const [isRun, setIsRun] = useState(false);
-  const [isFirstTurn, setIsFirstTurn] = useState(true);
-  const [isComeback, setIsComeback] = useState(false);
+const Timer = ({ initialTime, players, isStartGame }) => {
+    const [timeGame, setTimeGame] = useState(initialTime);
+    const [player, setPlayer] = useState(players);
+    const [isRun, setIsRun] = useState(false);
+    const [isFirstTurn, setIsFirstTurn] = useState(true);
+    const [isComeback, setIsComeback] = useState(false);
 
-  const idP = useRef(player.length - 1)
-  const [idPlayer,setIdPlayer] = useState(0)
-  const [bankActualPlayer, setBankActualPlayer] = useState(player[idPlayer]?.timerBank);
-  const [timeGameToMinute, setTimeGameToMinute] = useState(useSecondsToString(timeGame))
-  const [timeBankToMinute, setTimeBankToMinute] = useState(useSecondsToString(bankActualPlayer))
-  
-  const [passedTurnTime, setPassedTurnTime] = useState(initialTime)
-  const [passedTurnIdPlayer, setPassedTurnIdPlayer] = useState(idPlayer)
-  const [isComebackDisable, setIsComebackDisable] = useState(true)
-  
-  const [isButtonsDisable, setIsButtonsDisable] = useState(true)
+    const idP = useRef(player.length - 1);
+    const [idPlayer, setIdPlayer] = useState(0);
+    const [bankActualPlayer, setBankActualPlayer] = useState(
+        player[idPlayer]?.timerBank
+    );
+    const [timeGameToMinute, setTimeGameToMinute] = useState(
+        useSecondsToString(timeGame)
+    );
+    const [timeBankToMinute, setTimeBankToMinute] = useState(
+        useSecondsToString(bankActualPlayer)
+    );
 
-  useEffect(() => {
+    const [passedTurnTime, setPassedTurnTime] = useState(initialTime);
+    const [passedTurnIdPlayer, setPassedTurnIdPlayer] = useState(idPlayer);
+    const [isComebackDisable, setIsComebackDisable] = useState(true);
 
-    setBankActualPlayer(player[idPlayer]?.timerBank)
-    setTimeBankToMinute(useSecondsToString(player[idPlayer]?.timerBank));
+    const [isButtonsDisable, setIsButtonsDisable] = useState(true);
 
-  }, [idPlayer, player])
-  
+    useEffect(() => {
+        setBankActualPlayer(player[idPlayer]?.timerBank);
+        setTimeBankToMinute(useSecondsToString(player[idPlayer]?.timerBank));
+    }, [idPlayer, player]);
 
-  useEffect(() => {
-    let interval = null;
+    useEffect(() => {
+        let interval = null;
 
-    interval = setInterval(() => {
-      if (!isRun) {
-        return
-      }
-      if (timeGame > 0) {
-        setTimeGame((prev) => prev - 1);
-        setTimeGameToMinute(useSecondsToString(timeGame - 1))
-      } else {
-        setBankActualPlayer((prev) => prev - 1);
-        setTimeBankToMinute(useSecondsToString(bankActualPlayer))
-      }
-      if (bankActualPlayer <= 0 && timeGame <= 0) {
-        hanldeClickNextTurn();
-      }
-    }, 1000);
-    
-    return () => clearInterval(interval);
-  }, [timeGame, isRun, idPlayer, bankActualPlayer, player, idP, timeGameToMinute, timeBankToMinute]);
+        interval = setInterval(() => {
+            if (!isRun) {
+                return;
+            }
+            if (timeGame > 0) {
+                setTimeGame((prev) => prev - 1);
+                setTimeGameToMinute(useSecondsToString(timeGame - 1));
+            } else {
+                setBankActualPlayer((prev) => prev - 1);
+                setTimeBankToMinute(useSecondsToString(bankActualPlayer));
+            }
+            if (bankActualPlayer <= 0 && timeGame <= 0) {
+                hanldeClickNextTurn();
+            }
+        }, 1000);
 
-  const handleClickStart = () => {
-    setIsRun(!isRun);
-  };
+        return () => clearInterval(interval);
+    }, [
+        timeGame,
+        isRun,
+        idPlayer,
+        bankActualPlayer,
+        player,
+        idP,
+        timeGameToMinute,
+        timeBankToMinute,
+    ]);
 
-  const handleClickComebackTurn = () => {
-    setIsRun(true);
-    setIsButtonsDisable(true);
-    setIdPlayer(passedTurnIdPlayer);
-    setTimeGame(passedTurnTime);
-    setTimeGameToMinute(useSecondsToString(passedTurnTime));
-    setIsComebackDisable(true);
-  };
+    const handleClickStart = () => {
+        setIsRun(!isRun);
+    };
 
-  const handleClickButtonsDisable = () => {
-    setIsButtonsDisable(!isButtonsDisable);
-  };
+    const handleClickComebackTurn = () => {
+        setIsRun(true);
+        setIsButtonsDisable(true);
+        setIdPlayer(passedTurnIdPlayer);
+        setTimeGame(passedTurnTime);
+        setTimeGameToMinute(useSecondsToString(passedTurnTime));
+        setIsComebackDisable(true);
+    };
 
-  const hanldeClickReset = () => {
-    let id = idPlayer;
-    setIsButtonsDisable(true);
-    setVariablesTimeGameToInicial();
-    setBankActualPlayer(player[id].timerBank);
-    setTimeBankToMinute(useSecondsToString(player[id].timerBank))
-    setIsRun(true);
-  };
+    const handleClickButtonsDisable = () => {
+        setIsButtonsDisable(!isButtonsDisable);
+    };
 
-  const hanldeClickNextTurn = () => {
-    if (!isRun) {
-      setIsRun(true);
-      return
-    }
+    const hanldeClickReset = () => {
+        let id = idPlayer;
+        setIsButtonsDisable(true);
+        setVariablesTimeGameToInicial();
+        setBankActualPlayer(player[id].timerBank);
+        setTimeBankToMinute(useSecondsToString(player[id].timerBank));
+        setIsRun(true);
+    };
 
-    let notPassTurn = false;
-    let segPrev = 1;
-    notPassTurn = !notPassTurn ? (timeGame > (initialTime-segPrev) && initialTime != 0) : notPassTurn;
-    notPassTurn = !notPassTurn && initialTime === 0 ? (bankActualPlayer > (player[idPlayer]?.timerBank-segPrev) && player[idPlayer]?.timerBank != 0) : notPassTurn;
-    if (notPassTurn) {
-      return
-    }
-
-    // Almacena informacion para handleClickComebackTurn
-    setVariablesPassedTurn();
-    // Reinicia timer de turno
-    setVariablesTimeGameToInicial();
-    // Bloquea los botones
-    setIsButtonsDisable(true);
-
-    let id = idPlayer;
-
-    if (!isFirstTurn) {
-      if (idP.current === idPlayer) {
-        id = 0;
-      } else {
-        id = idPlayer + 1;
-      }
-    } else {
-      if (idP.current === idPlayer && !isComeback) {
-        if (!isComeback) {
-          id = idPlayer;
-          setIsComeback(true);
+    const hanldeClickNextTurn = () => {
+        if (!isRun) {
+            setIsRun(true);
+            return;
         }
-      } else {
-        if (isComeback) {
-          id = idPlayer - 1;
-          if (id === -1) {
-            id = 0;
-            setIsFirstTurn(false);
-          }
+
+        let notPassTurn = false;
+        let segPrev = 1;
+        notPassTurn = !notPassTurn
+            ? timeGame > initialTime - segPrev && initialTime != 0
+            : notPassTurn;
+        notPassTurn =
+            !notPassTurn && initialTime === 0
+                ? bankActualPlayer > player[idPlayer]?.timerBank - segPrev &&
+                  player[idPlayer]?.timerBank != 0
+                : notPassTurn;
+        if (notPassTurn) {
+            return;
+        }
+
+        // Almacena informacion para handleClickComebackTurn
+        setVariablesPassedTurn();
+        // Reinicia timer de turno
+        setVariablesTimeGameToInicial();
+        // Bloquea los botones
+        setIsButtonsDisable(true);
+
+        let id = idPlayer;
+
+        if (!isFirstTurn) {
+            if (idP.current === idPlayer) {
+                id = 0;
+            } else {
+                id = idPlayer + 1;
+            }
         } else {
-          id = idPlayer + 1;
+            if (idP.current === idPlayer && !isComeback) {
+                if (!isComeback) {
+                    id = idPlayer;
+                    setIsComeback(true);
+                }
+            } else {
+                if (isComeback) {
+                    id = idPlayer - 1;
+                    if (id === -1) {
+                        id = 0;
+                        setIsFirstTurn(false);
+                    }
+                } else {
+                    id = idPlayer + 1;
+                }
+            }
         }
-      }
-    }
-    updateBankPlayer(idPlayer, bankActualPlayer);
-    setIdPlayer(id);
-    setBankActualPlayer(player[id].timerBank);
-    setTimeBankToMinute(useSecondsToString(bankActualPlayer))
-  };
+        updateBankPlayer(idPlayer, bankActualPlayer);
+        setIdPlayer(id);
+        setBankActualPlayer(player[id].timerBank);
+        setTimeBankToMinute(useSecondsToString(bankActualPlayer));
+    };
 
-  const updateBankPlayer = (playerId, bankAP) => {
-    const newBankTimePlayer = player.map((player) => {
-      if (player.id === playerId) {
-        return {
-          ...player,
-          timerBank: bankAP,
-        };
-      }
-      return player;
-    });
-    setPlayer(newBankTimePlayer);
-  };
+    const updateBankPlayer = (playerId, bankAP) => {
+        const newBankTimePlayer = player.map((player) => {
+            if (player.id === playerId) {
+                return {
+                    ...player,
+                    timerBank: bankAP,
+                };
+            }
+            return player;
+        });
+        setPlayer(newBankTimePlayer);
+    };
 
-  const setVariablesPassedTurn = () => {
-  // const setVariablesPassedTurn = (timeGame) => {
-    setPassedTurnIdPlayer(idPlayer);
-    setPassedTurnTime(timeGame);
-    setIsComebackDisable(false);
-  };
-  
-  const setVariablesTimeGameToInicial = () => {
-    setTimeGame(initialTime);
-    setTimeGameToMinute(useSecondsToString(initialTime));
-  };
+    const setVariablesPassedTurn = () => {
+        // const setVariablesPassedTurn = (timeGame) => {
+        setPassedTurnIdPlayer(idPlayer);
+        setPassedTurnTime(timeGame);
+        setIsComebackDisable(false);
+    };
 
-  return (
-    <div className="containerStartGame">
-      <motion.button
-        whileTap={{ scale: 0.99 }}
-        className="game"
-        onClick={hanldeClickNextTurn}
-      >
-        <div
-          className="timer"
-          style={{ backgroundColor: player[idPlayer].color,
-            height : 'auto'
-          }}
-        >
-          <span 
-            style={
-              timeGame > 0 ? { fontSize : "6rem", fontFamily: "'Inconsolata', monospace", fontWeight: 900, transition: "font-size 0.15s ease-in-out"}:
-              { fontSize : "4rem", fontFamily: "'Inconsolata', monospace", fontWeight: 900, transition: "font-size 1s ease-in-out"}
-            }>{timeGameToMinute}
-          </span> 
-          <span 
-            style={
-              timeGame > 0 ? { fontSize : "4rem", fontFamily: "'Inconsolata', monospace", fontWeight: 900, transition: "font-size 0.15s ease-in-out"}:
-              { fontSize : "6rem", fontFamily: "'Inconsolata', monospace", fontWeight: 900, transition: "font-size 1s ease-in-out"}
-            }>{timeBankToMinute}
-          </span>
+    const setVariablesTimeGameToInicial = () => {
+        setTimeGame(initialTime);
+        setTimeGameToMinute(useSecondsToString(initialTime));
+    };
+
+    return (
+        <div className="containerStartGame">
+            <motion.button
+                whileTap={{ scale: 0.99 }}
+                className="game"
+                onClick={hanldeClickNextTurn}
+            >
+                <div
+                    className="timer"
+                    style={{
+                        backgroundColor: player[idPlayer].color,
+                        height: "auto",
+                    }}
+                >
+                    <span
+                        style={
+                            timeGame > 0
+                                ? {
+                                      fontSize: "6rem",
+                                      fontFamily: "'Inconsolata', monospace",
+                                      fontWeight: 900,
+                                      transition: "font-size 0.15s ease-in-out",
+                                  }
+                                : {
+                                      fontSize: "4rem",
+                                      fontFamily: "'Inconsolata', monospace",
+                                      fontWeight: 900,
+                                      transition: "font-size 1s ease-in-out",
+                                  }
+                        }
+                    >
+                        {timeGameToMinute}
+                    </span>
+                    <span
+                        style={
+                            timeGame > 0
+                                ? {
+                                      fontSize: "4rem",
+                                      fontFamily: "'Inconsolata', monospace",
+                                      fontWeight: 900,
+                                      transition: "font-size 0.15s ease-in-out",
+                                  }
+                                : {
+                                      fontSize: "6rem",
+                                      fontFamily: "'Inconsolata', monospace",
+                                      fontWeight: 900,
+                                      transition: "font-size 1s ease-in-out",
+                                  }
+                        }
+                    >
+                        {timeBankToMinute}
+                    </span>
+                </div>
+                <Players
+                    players={player}
+                    playerId={idPlayer}
+                    timeBankToMinute={timeBankToMinute}
+                    isStartGame={isStartGame}
+                />
+            </motion.button>
+            <div className="buttonsGame">
+                <button onClick={handleClickStart} style={styleButtonsTimer()}>
+                    {isRun ? (
+                        <img
+                            style={{ width: "5rem", height: "5rem" }}
+                            src="./public/buttonPause.svg"
+                            alt="Pause"
+                        />
+                    ) : (
+                        <img
+                            style={{ width: "5rem", height: "5rem" }}
+                            src="./public/buttonPlay.svg"
+                            alt="Play"
+                        />
+                    )}
+                </button>
+                <button
+                    onClick={hanldeClickReset}
+                    disabled={isButtonsDisable}
+                    style={styleButtonsTimer(isButtonsDisable)}
+                >
+                    <img
+                        style={{ width: "5rem", height: "5rem" }}
+                        src="./public/buttonReset.svg"
+                        alt="Reset"
+                    />
+                </button>
+                <button
+                    onClick={handleClickComebackTurn}
+                    disabled={isComebackDisable || isButtonsDisable}
+                    style={styleButtonsTimer(
+                        isComebackDisable || isButtonsDisable
+                    )}
+                >
+                    <img
+                        style={{ width: "5rem", height: "5rem" }}
+                        src="./public/buttonComebackTurn.svg"
+                        alt="Previous Turn"
+                    />
+                </button>
+                <button
+                    onClick={handleClickButtonsDisable}
+                    style={styleButtonsTimer()}
+                >
+                    {isButtonsDisable ? (
+                        <img
+                            style={{ width: "5rem", height: "5rem" }}
+                            src="./public/buttonUnlock.svg"
+                            alt="Unlocked"
+                        />
+                    ) : (
+                        <img
+                            style={{ width: "5rem", height: "5rem" }}
+                            src="./public/buttonLock.svg"
+                            alt="Locked"
+                        />
+                    )}
+                </button>
+            </div>
         </div>
-        <Players
-          players={player}
-          playerId={idPlayer}
-          timeBankToMinute={timeBankToMinute}
-          renderPlayer={renderPlayer}
-        />
-      </motion.button>
-      <div className="buttonsGame">
-        <button
-          onClick={handleClickStart}
-          style={styleButtonsTimer()}
-        >
-          {isRun ?
-          <img style={{ width: '5rem', height: '5rem' }} src="./public/buttonPause.svg" alt="Pause" />
-          : <img style={{ width: '5rem', height: '5rem' }} src="./public/buttonPlay.svg" alt="Play" />}
-        </button>
-        <button 
-          onClick={hanldeClickReset}
-          disabled={isButtonsDisable}
-          style={styleButtonsTimer(isButtonsDisable)}
-        >
-          <img style={{ width: '5rem', height: '5rem' }} src="./public/buttonReset.svg" alt="Reset" />
-        </button>
-        <button
-          onClick={handleClickComebackTurn}
-          disabled={isComebackDisable || isButtonsDisable}
-          style={styleButtonsTimer((isComebackDisable || isButtonsDisable))}
-        >
-          <img style={{ width: '5rem', height: '5rem' }} src="./public/buttonComebackTurn.svg" alt="Previous Turn" />
-        </button>
-        <button
-          onClick={handleClickButtonsDisable}
-          style={styleButtonsTimer()}
-        >
-          { isButtonsDisable ?
-          <img style={{ width: '5rem', height: '5rem' }} src="./public/buttonUnlock.svg" alt="Unlocked" />
-          :<img style={{ width: '5rem', height: '5rem' }} src="./public/buttonLock.svg" alt="Locked" />
-        }
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Timer;
