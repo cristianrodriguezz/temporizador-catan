e4fc34fa869c67a0ae5e7130a8548da436796476
adrodriguez <adrodriguez@labsxd.com>
2023-04-02 02:00:46 -0300

    fix colors

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,10 +1,10 @@
 
 export const colors = [
-    { id: 1, value: "red", label: "Red" },
-    { id: 3, value: "blue", label: "Blue" },
-    { id: 4, value: "#ffffff97", label: "White" },
-    { id: 5, value: "orange", label: "Orange" },
-    { id: 2, value: "green", label: "Green" },
-    { id: 6, value: "#78270e", label: "Brown" },
+    { id: 1, value: "#b70000", label: "Red" },
+    { id: 3, value: "#0057bb", label: "Blue" },
+    { id: 4, value: "#b3b3b3", label: "White" },
+    { id: 5, value: "#e89f00", label: "Orange" },
+    { id: 2, value: "#4bb749", label: "Green" },
+    { id: 6, value: "#8b4513", label: "Brown" },
   ];
 
