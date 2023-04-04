4ef275da5e2600ac80e3aa5740f7f0be3ae75848
adrodriguez <adrodriguez@labsxd.com>
2023-04-03 21:08:29 -0300

    fix componente style

--- src/constants/styleTimer.js
+++ src/constants/styleTimer.js
@@ -1,34 +1,34 @@
 export const styleTimerTurn = (initialTime, timeGame, bankActualPlayer) => {
-    if (initialTime === 0) {
-        return {...fontTimer, fontSize: "0rem", transition: "font-size 1s ease-in-out", };
+    if (initialTime > 0) {
+        if (timeGame > 0) {
+            return { ...fontTimer, fontSize: "7rem", transition: "font-size 0.15s ease-in-out", };
+        }
+        else {
+            return { ...fontTimer, fontSize: "4rem", transition: "font-size 1s ease-in-out", };
+        }
     }
     // else if (bankActualPlayer === 0) {
     //     return {...fontTimer, fontSize: "11rem", transition: "font-size 1s ease-in-out", };
     // }
-    else{
-        if (timeGame > 0) {
-            return {...fontTimer, fontSize: "7rem", transition: "font-size 0.15s ease-in-out", };
-        }
-        else{
-            return {...fontTimer, fontSize: "4rem", transition: "font-size 1s ease-in-out", };
-        }
+    else {
+        return { ...fontTimer, fontSize: "0rem", transition: "font-size 1s ease-in-out", };
     }
 };
 
 export const styleTimerBank = (initialTime, timeGame, bankActualPlayer) => {
-    if (initialTime === 0) {
-        return {...fontTimer, fontSize: "8.5rem", transition: "font-size 1s ease-in-out", };
+    if (initialTime > 0) {
+        if (timeGame > 0) {
+            return { ...fontTimer, fontSize: "4rem", transition: "font-size 0.15s ease-in-out", };
+        }
+        else {
+            return { ...fontTimer, fontSize: "7rem", transition: "font-size 1s ease-in-out", };
+        }
     }
     // else if (bankActualPlayer === 0) {
     //     return {...fontTimer, fontSize: "0rem", transition: "font-size 1s ease-in-out", };
     // }
-    else{
-        if (timeGame > 0) {
-            return {...fontTimer, fontSize: "4rem", transition: "font-size 0.15s ease-in-out",  };
-        }
-        else{
-            return {...fontTimer, fontSize: "7rem", transition: "font-size 1s ease-in-out",  };
-        }
+    else {
+        return { ...fontTimer, fontSize: "8.5rem", transition: "font-size 1s ease-in-out", };
     }
 };
 
