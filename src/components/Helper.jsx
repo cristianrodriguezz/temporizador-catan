f5fd32a16b6c010e29f987f3e1f00680fecca741
Cristian Rodriguez <cristiandosespigas@gmail.com>
2024-04-09 16:08:03 -0300

    animation info

--- src/components/Helper.jsx
+++ src/components/Helper.jsx
@@ -2,7 +2,7 @@
 
 const Helper = () => {
   return (
-    <div className="helper">
+    <div className="helper animate-open">
       <p>
         Cada jugador tiene su propio <strong >Banco de tiempo</strong>. Se consume una vez finalizado el <strong>Tiempo de turno</strong>.
       </p>
