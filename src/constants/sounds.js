e876eb78424ad183d4045317bada238f31148e1d
Cristian Rodriguez <cristiandosespigas@gmail.com>
2024-04-09 15:23:55 -0300

    fix sound

--- src/constants/sounds.js
+++ src/constants/sounds.js
@@ -1,27 +1,34 @@
-import soundFinishTime11 from "../assets/soundFinishTime1.wav"
-import soundFinishTime12 from "../assets/soundFinishTime2.wav"
-import soundFinishTimeGame1 from "../assets/soundFinishTimeGame1.wav"
-import soundFinishTimeGame2 from "../assets/soundFinishTimeGame2.wav"
-import soundPassTurn2 from "../assets/soundPassTurn2.wav"
-import soundGeneralClick from "../assets/soundGeneralClick.wav"
+import soundFinishTime11Src from "../assets/soundFinishTime1.wav";
+import soundFinishTime12Src from "../assets/soundFinishTime2.wav";
+import soundFinishTimeGame1Src from "../assets/soundFinishTimeGame1.wav";
+import soundFinishTimeGame2Src from "../assets/soundFinishTimeGame2.wav";
+import soundPassTurn2Src from "../assets/soundPassTurn2.wav";
+import soundGeneralClickSrc from "../assets/soundGeneralClick.wav";
+
+const soundFinishTime11 = new Audio(soundFinishTime11Src);
+const soundFinishTime12 = new Audio(soundFinishTime12Src);
+const soundFinishTimeGame1 = new Audio(soundFinishTimeGame1Src);
+const soundFinishTimeGame2 = new Audio(soundFinishTimeGame2Src);
+const soundPassTurn2 = new Audio(soundPassTurn2Src);
+const soundGeneralClick = new Audio(soundGeneralClickSrc);
 
 export const play = (sound) => {
   if (sound === "soundFinishTime11") {
-    new Audio(soundFinishTime11).play();
+    soundFinishTime11.play();
   }
   if (sound === "soundFinishTime12") {
-    new Audio(soundFinishTime12).play();
+    soundFinishTime12.play();
   }
   if (sound === "soundFinishTimeGame1") {
-    new Audio(soundFinishTimeGame1).play();
+    soundFinishTimeGame1.play();
   }
   if (sound === "soundFinishTimeGame2") {
-    new Audio(soundFinishTimeGame2).play();
+    soundFinishTimeGame2.play();
   }
   if (sound === "soundPassTurn2") {
-    new Audio(soundPassTurn2).play();
+    soundPassTurn2.play();
   }
   if (sound === "soundGeneralClick") {
-    new Audio(soundGeneralClick).play();
+    soundGeneralClick.play();
   }
 };
